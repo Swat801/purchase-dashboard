c554ed72053e5cc30f3382c59fceeec986f26023
Shashwat Singh <singhswat801@gmail.com>
2026-07-25 12:33:02 +0530

    Update material name for liion in purchase.js

--- purchase.js
+++ purchase.js
@@ -103,7 +103,7 @@ const purchaseData = {
     liion: {
         materials: [
             {
-                name: "Lithium Carbonate Aur kuch bhi aur nhi",
+                name: "Lithium Carbonate",
                 color: "#6C4CFF",
                 unit: "₹/kg",
                 suppliers: [
